2958f4b93840b52d467fb00b6b1096bf9dd091e6
Dell PC <monu40128@gmail.com>
2022-10-06 14:32:07 +0530

    ..

--- web scraping(download all images of the site).py
+++ web scraping(download all images of the site).py
@@ -1,3 +1,7 @@
+import os
+
+from sphinx.util import requests
+
 main_url='https://www.ducatindia.com'
 all_apps=[]
 resp=requests.get(main_url)
@@ -6,7 +10,7 @@ anchor_data=soap.find_all('a')
 count=0
 for i in anchor_data:
     url=i.get('href')
-    if((url.startswith('https') or url.startswith('http')) and 'www.ducatindia.com' in url):
+    if (url.startswith('https') or url.startswith('http')) and 'www.ducatindia.com' in url:
         all_apps.append(url)
 
 for i in all_apps:
@@ -15,13 +19,13 @@ for i in all_apps:
     anchor=soap.find_all('img')
     for j in anchor:
         imgs_url=j.get('src')
-        if(not imgs_url.startswith('https://')):
+        if not imgs_url.startswith('https://'):
             imgs_url=main_url+imgs_url
-        if(main_url in imgs_url):
+        if main_url in imgs_url:
             img_name=imgs_url[imgs_url.rindex('/')+1:]
             req=requests.get(imgs_url)
             img_bytes=req.content
-            if(not os.path.isdir('images2')):
+            if not os.path.isdir('images2'):
                 os.mkdir('images2')
             try:    
                 file=open(f'images2/{img_name}','wb')
